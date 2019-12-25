b6a2e5fc5c4b253ff8346e1c1295856edf800c0e
Administrator <997797281@qq.com>
2019-12-25 18:37:55 +0800

    订单样式修改

--- miniprogram/pages/mine/order/order.js
+++ miniprogram/pages/mine/order/order.js
@@ -5,11 +5,50 @@ Page({
     list:[]
   },
 
+  onChange(event)
+  {
+    var that = this;
+    var tabName = event.detail.name;
+    console.log(tabName);
+    var payStatus;
+    switch(tabName)
+    {
+      case 1:
+        payStatus = 0;//0未付款
+        break;
+      case 2:
+        payStatus = 1;//1已付款的，等待发货和收货
+        break;
+      case 3:
+        payStatus = 3;//3已签收，订单完成
+        break;
+      case 4:
+        payStatus = -4;//-4订单已取消
+        break;
+      default:
+        payStatus = "";
+        //全部，不传
+        break;
+    }
+    wx.request({
+      url: app.baseUrl + '/orderList',
+      data:{
+        userId:288,
+        status:payStatus
+      },
+      method:'GET',
+      success(res) {
+        that.setData({list:res.data.data});
+          console.log(res.data.data);
+      }
+    })
+  },
+
   onLoad: function (options) {
       //根据用户点击的是什么按钮，决定加载到哪个页面
       var that = this;
       var id = options.id;
-      var payStatus = 0;
+      var payStatus = "";
       switch(id) {
         case 'unPayClick':
           this.setData({active:1});
