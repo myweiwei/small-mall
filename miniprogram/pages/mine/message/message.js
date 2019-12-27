c54a89108f6f7974773dba38efd1f82fda6a11c5
Administrator <997797281@qq.com>
2019-12-27 19:13:06 +0800

    消息

---
+++ miniprogram/pages/mine/message/message.js
@@ -0,0 +1,74 @@
+const app = getApp();
+const tapItemArray = [];
+Page({
+  /**
+   * 页面的初始数据
+   */
+
+  onClick:function(){
+    wx.requestSubscribeMessage({
+      tmplIds: ['ip3gxMy-kzwM9pzEmGl3t-BMJBSlPeiH1Z-8JJHzCsQ'],
+      success (res) { 
+
+      }
+    })
+  },
+  data: {
+      
+  },
+ 
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
