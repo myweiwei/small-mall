54ce4cc601318f8740865347972eed01a64f96a8
liuyuwei <1349541630@qq.com>
2019-12-29 20:41:04 +0800

    购物车模块

--- miniprogram/pages/car/car.js
+++ miniprogram/pages/car/car.js
@@ -9,7 +9,71 @@ Page({
     ],
     baseUrl: '',
     userId: '',
-    name:"dx"
+    show:false,
+    popData:'',
+    number:1,
+    id:'',
+    show1:false,
+    editFlag:false,
+    allChecked:false,
+    checkArr:[],
+  },
+  toIndex:function(){
+    console.log(1);
+    wx.reLaunch({
+      url: '/pages/index/index'
+    });
+  },
+  editFunc:function(e){
+    let me=this;
+    me.setData({
+      editFlag:true
+    })
+  },
+  onChange:function(e){
+    let me=this;
+    me.setData({
+      number:e.detail
+    })
+  },
+  showPopup:function(e){
+    let me=this;
+    me.setData({
+      show:true,
+      number: e.target.dataset.item.number,
+      id: e.target.dataset.item.productId
+    })
+  },
+  onClose: function () {
+    let me = this;
+    me.setData({
+      show: false
+    })
+  },
+  addCar:function(e){
+    let me=this;
+    me.setData({
+      show1:true
+    })
+    wx.request({
+      url: me.data.baseUrl + '/shopcar/product',
+      method: "POST",
+      data: {
+        userId:me.data.userId,
+        productId: e.target.dataset.id,
+        number:me.data.number
+      },
+      success: function (data) {
+        me.setData({
+          show1: false,
+          show:false
+        })
+        me.getCarData();
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    })
   },
   getUser: function () {
     var me = this;
@@ -24,7 +88,6 @@ Page({
               code: res.code
             },
             success: function (data) {
-              console.log(data);
               me.setData({
                userId:data.data.data
               },()=>{
@@ -40,29 +103,199 @@ Page({
         }
       }
     });
-    // wx.getSetting({
-    //   success: function (res) {
-    //     if (res.authSetting['scope.userInfo']) {
-    //       // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-    //       wx.getUserInfo({
-    //         success: function (res) {
-    //           console.log(res.userInfo)
-    //         }
-    //       })
-    //     }
-    //     else {
-    //       that.setData({ show1: true })
-    //     }
-    //   }
-    // })
   },
   checkboxChange:function(e){
-    console.log('radio发生change事件，携带value值为：', e.detail.value)
+    let me=this;
+    me.setData({
+      show1: true
+    })
+    if (e.detail.value[0]){
+      wx.request({
+        url: me.data.baseUrl + '/shopcar/checkOne',
+        method: "POST",
+        data: {
+          userId: me.data.userId,
+          productId: parseInt(e.detail.value[0]),
+        },
+        success: function (data) {
+          me.getCarData();
+          me.setData({
+            show1: false
+          })
+        },
+        error: function (err) {
+          console.log(err);
+        }
+      })
+    }
+    else {
+      wx.request({
+        url: me.data.baseUrl + '/shopcar/unCheckOne',
+        method: "POST",
+        data: {
+          userId: me.data.userId,
+          productId: parseInt(e.target.dataset.id),
+        },
+        success: function (data) {
+          me.getCarData();
+          me.setData({
+            show1: false
+          })
+        },
+        error: function (err) {
+          console.log(err);
+        }
+      })
+    }
+  },
+  checkboxChange1:function(e){
+    let me=this;
+    let arr = me.data.carData;
+    if(e.detail.value.length){
+      for (var i = 0; i < arr.cartProductVOList.length;i++){
+        if (arr.cartProductVOList[i].productId == e.detail.value[0]) {
+          arr.cartProductVOList[i].checkFlag=true;
+        }
+      }
+    }
+    else {
+      let id = e.target.dataset.id;
+      for (var i = 0; i < arr.cartProductVOList.length; i++)       {
+        if (arr.cartProductVOList[i].productId == id) {
+          arr.cartProductVOList[i].checkFlag = false;
+        }
+      }
+      me.setData({
+        allChecked:false
+      })
+    }
+    me.setData({
+      carData:arr
+    })
+    let arr1=[];
+    for (let i = 0; i < me.data.carData.cartProductVOList.length;i++){
+      if (me.data.carData.cartProductVOList[i].checkFlag){
+        arr1.push(me.data.carData.cartProductVOList[i].productId);
+      }
+    }
+    me.setData({
+      checkArr:arr1
+    })
+    if(me.data.checkArr.length==me.data.carData.cartProductVOList.length){
+      me.setData({
+        allChecked:true
+      })
+    }
+  },
+  allcheckboxChange:function(e){
+    let me = this;
+    me.setData({
+      show1: true
+    })
+    if (e.detail.value.length) {
+      wx.request({
+        url: me.data.baseUrl + '/shopcar/checkAll',
+        method: "POST",
+        data: {
+          userId: me.data.userId
+        },
+        success: function (data) {
+          me.getCarData();
+          me.setData({
+            show1: false
+          })
+        },
+        error: function (err) {
+          console.log(err);
+        }
+      })
+    }
+    else {
+      wx.request({
+        url: me.data.baseUrl + '/shopcar/unCheckAll',
+        method: "POST",
+        data: {
+          userId: me.data.userId
+        },
+        success: function (data) {
+          me.getCarData();
+          me.setData({
+            show1: false
+          })
+        },
+        error: function (err) {
+          console.log(err);
+        }
+      })
+    }
+  },
+  allcheckboxChange1:function(e){
+    let me=this;
+    let arr1 = [];
+    if(e.detail.value.length){
+      let list=me.data.carData;
+      for (var i = 0; i < list.cartProductVOList.length;i++)      {
+        list.cartProductVOList[i].checkFlag=true;
+        arr1.push(list.cartProductVOList[i].productId);
+      }
+      me.setData({
+        carData:list,
+        checkArr: arr1,
+      })
+    }
+    else {
+      let list = me.data.carData;
+      for (var i = 0; i < list.cartProductVOList.length; i++)     {
+        list.cartProductVOList[i].checkFlag = false;
+      }
+      me.setData({
+        carData: list,
+        checkArr:[]
+      })
+    }
+  },
+  delFunc:function(){
+    let me=this;
+    me.setData({
+      show1: true
+    })
+    wx.request({
+      url: me.data.baseUrl + '/shopcar/product/ ',
+      method: "DELETE",
+      data: {
+        userId: me.data.userId,
+        productIds:me.data.checkArr,
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded',
+      },
+      success: function (data) {
+        wx.showToast({
+          title: '删除成功',
+          icon: 'none',
+          duration: 2000
+        })
+        me.getCarData();
+        me.setData({
+          show1: false,
+          editFlag:false
+        })
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    })
+  },
+  saveFunc:function(){
+    let me=this;
+    me.setData({
+      editFlag:false
+    })
   },
   getCarData:function(){
       let me = this;
-      wx.showLoading({
-        title: '加载中',
+      me.setData({
+        show1: true
       })
       wx.request({
         url: me.data.baseUrl + '/shopcar/products',
@@ -74,16 +307,42 @@ Page({
           'content-type': 'application/json' // 默认值
         },
         success(res) {
+          for (var i = 0; i < res.data.data.cartProductVOList.length;i++){
+            res.data.data.cartProductVOList[i].checkFlag=false;
+          }
           me.setData({
-            carData: res.data.data
+            carData: res.data.data,
+            show1:false
           })
-          console.log(res.data.data);
           wx.hideLoading();
         },
         error: function (err) {
           console.log(err);
         }
       })
+  },
+  jia:function(e){
+    let me = this;
+    me.setData({
+      show1: true
+    })
+    wx.request({
+      url: me.data.baseUrl + '/shopcar/updateProduct/' + parseInt(e.target.dataset.operate),
+      method: "POST",
+      data: {
+        userId: me.data.userId,
+        productId: e.target.dataset.id,
+      },
+      success: function (data) {
+        me.getCarData();
+        me.setData({
+          show1: false
+        })
+      },
+      error: function (err) {
+        console.log(err);
+      }
+    })
   },
   /**
    * 生命周期函数--监听页面加载
