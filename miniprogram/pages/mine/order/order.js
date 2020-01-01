aeb313206d27ad9fb005a77cc6a9050f407fde4b
lilei <997797281@qq.com>
2020-01-01 22:49:15 +0800

    薇改购物车地址订单bug

--- miniprogram/pages/mine/order/order.js
+++ miniprogram/pages/mine/order/order.js
@@ -2,12 +2,13 @@ const app = getApp();
 Page({ 
   data: {
     active: 0,
-    list:[]
+    list:[],
+    id:'',
+    payStatus:''
   },
 
   goOrderDetail:function(event)
   {
-      console.log(event.currentTarget.dataset.id);
       var orderNo = event.currentTarget.dataset.id;
       wx.navigateTo({
         url: '/pages/mine/order/orderdetail/orderdetail?orderNo=' + orderNo
@@ -18,27 +19,86 @@ Page({
   {
     var that = this;
     var tabName = event.detail.name;
-    console.log(tabName);
     var payStatus;
+    console.log(tabName);
     switch(tabName)
     {
       case 1:
         payStatus = 0;//0未付款
+        that.setData({
+          id:'unPayClick'
+        })
         break;
       case 2:
         payStatus = 1;//1已付款的，等待发货和收货
+        that.setData({
+          id: 'send'
+        })
         break;
       case 3:
         payStatus = 3;//3已签收，订单完成
+        that.setData({
+          id: 'say'
+        })
         break;
       case 4:
         payStatus = -4;//-4订单已取消
+        that.setData({
+          id: 'cancel'
+        })
         break;
       default:
         payStatus = "";
+        that.setData({
+          id: 'myOrder'
+        })
         //全部，不传
         break;
     }
+    that.getData(payStatus);
+  },
+  getPayStatus(id){
+    let me=this;
+    console.log(id);
+    switch (id) {
+      case 'unPayClick':
+        me.setData({ 
+          active: 1 ,
+          payStatus:0
+        });//0未付款
+        break;
+      case 'send':
+        me.setData({ 
+          active: 2,
+          payStatus: 1 
+        });//1已付款的，等待发货和收货
+        break;
+      case 'say':
+        me.setDatame.setData({
+          active: 3,
+          payStatus: 3
+        });//3已签收，订单完成
+        break;
+      case 'myOrder':
+        //全部订单不用传
+        me.setData({ 
+          active: 0,
+          payStatus: ''
+        });
+        break;
+      case 'cancel':
+        //全部订单不用传
+        me.setData({ active: 4 });
+        break;
+    }
+    console.log(me.data.payStatus);
+    me.getData(me.data.payStatus)
+  },
+  getData: function (payStatus){
+    let that=this;
+    wx.showLoading({
+      title: '加载中',
+    }),
     wx.request({
       url: app.baseUrl + '/orderList',
       data:{
@@ -47,53 +107,26 @@ Page({
       },
       method:'GET',
       success(res) {
+        wx.hideLoading();
         for (let i = 0; i < res.data.data.length;i++){
           res.data.data[i].price =( res.data.data[i].price/100).toFixed(2);
           var b = res.data.data[i].price.split(".");
           res.data.data[i].zs = b[0];
           res.data.data[i].xs = b[1];
-          res.data
         }
         that.setData({ list: res.data.data });
       }
     })
   },
-
   onLoad: function (options) {
       //根据用户点击的是什么按钮，决定加载到哪个页面
       var that = this;
-      var id = options.id;
-      var payStatus = "";
-      switch(id) {
-        case 'unPayClick':
-          this.setData({active:1});
-          payStatus = 0;//0未付款
-          break;
-        case 'send':
-          this.setData({active:2});
-          payStatus = 1;//1已付款的，等待发货和收货
-          break;
-        case 'say':
-          this.setData({active:3});
-          payStatus = 3;//3已签收，订单完成
-          break;
-        case 'myOrder':
-          //全部订单不用传
-          this.setData({active:0});
-          break;
-      }
-      console.log(payStatus);
-      wx.request({
-        url: app.baseUrl + '/orderList',
-        data:{
-          userId:288,
-          status:payStatus
-        },
-        method:'GET',
-        success(res) {
-          that.setData({list:res.data.data});
-            console.log(res.data.data);
-        }
+      that.setData({
+        id:options.id
       })
+  },
+  onShow: function (){
+    let that=this;
+    that.getPayStatus(that.data.id);
   }
 })
