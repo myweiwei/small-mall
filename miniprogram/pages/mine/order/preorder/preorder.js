ab3e6b5bbec27f3dc74e7d52895a53d9f0d645ad
liuyuwei <1349541630@qq.com>
2020-01-05 23:19:13 +0800

    订单页面

--- miniprogram/pages/mine/order/preorder/preorder.js
+++ miniprogram/pages/mine/order/preorder/preorder.js
@@ -2,95 +2,93 @@ const app = getApp();
 var orderNo;
 Page({
   data: {
-    active: 0,
-    obj: {},
-    time: 0, //倒计时剩余时间
-    timeData: {}//onChange方法取time的时间，给timeData用
+    addressDate:[],
+    chooseAddress:{}
   },
 
-  onChange(e) {
-    var that = this;
-    if (e.detail.hours == 0 && e.detail.minutes == 0 && e.detail.seconds == 0) {
-      //重新加载该订单状态
-      wx.request({
-        url: app.baseUrl + '/order',
-        data: {
-          orderNo: orderNo
-        },
-        method: 'GET',
-        success(res) {
-          console.log("订单支付时间超时，重新请求数据刷新页面" + res.data.data);
-          getData(res, that);
-        }
-      })
-
-    }
-    this.setData({
-      timeData: e.detail
-    });
-    console.log(e.detail);
-  },
-
-  onLoad: function (options) {
-    var that = this;
-    orderNo = options.orderNo;
-    wx.request({
-      url: app.baseUrl + '/order',
-      data: {
-        orderNo: orderNo
-      },
-      method: 'GET',
-      success(res) {
-        getData(res, that);
-        console.log(res.data.data + "----------------------");
-      }
+  getUser: function () {
+    let me = this;
+    wx.showLoading({
+      title: '加载中',
     })
+    wx.login({
+      success(res) {
+        if (res.code) {
+          //通过wx.login内置函数，得到临时code码
+          wx.request({
+            url: app.baseUrl + '/openIdSessionKey',
+            method: "get",
+            data: {
+              code: res.code
+            },
+            success: function (data) {
+              me.setData({
+                userId: data.data.data
+              })
+              me.getAddress();
+              wx.hideLoading();
+            },
+            error: function (err) {
+              console.log(err);
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    });
   },
-  cancelOrder: function () {
-    var that = this;
+  getAddress: function () {
+    let me = this;
     wx.request({
-      url: app.baseUrl + '/cancelOrder',
+      url: app.baseUrl + '/address',
+      data: { userId: me.data.userId },
+      method: 'GET',
+      success: function (res) {
+        let v = res.data.data.find((value) => value.defaults == 1); 
+        me.setData({
+          addressDate: res.data.data,
+          chooseAddress: v!='undefined' ? v : res.data.data[0]
+        })
+        console.log(v);
+      }
+    });
+  },
+  onLoad: function (options) {
+    let me = this;
+    me.getUser();
+    
+  },
+  payFunc:function(){
+    wx.request({
+      url:'http://192.168.1.5:8888/order',
       data: {
-        userId: 288,
-        orderNo: orderNo
+        userId:288,
+        addressId:102
       },
       header: {
         'content-type': 'application/x-www-form-urlencoded',
       },
       method: 'POST',
       success(res) {
-        wx.request({
-          url: app.baseUrl + '/order',
-          data: {
-            orderNo: orderNo
-          },
-          method: 'GET',
-          success(res) {
-            getData(res, that);
-          }
-        })
+       console.log(res.data);
+        wx.requestPayment(
+          {
+            timeStamp: res.data.data.payParam.timeStamp,
+            nonceStr: res.data.data.payParam.nonceStr,
+            package: res.data.data.payParam.package,
+            signType: res.data.data.payParam.signType,
+            paySign: res.data.data.payParam.paySign,
+            success: function (res) {
+              console.log(res.data);
+            },
+            fail: function (res) {
+              console.log(res);
+             },
+            complete: function (res) { }
+          })
       }
     })
   }
 })
 
-function getData(res, that) {
-  var order = res.data.data;
-  that.setData({ time: order.waitPayTime });
-
-  var a = (order.price / 100).toFixed(2).split('.');
-  order.zs = a[0];
-  order.xs = a[1];
-
-
-  var orderDetail = res.data.data.orderItem;
-  for (let i = 0; i < orderDetail.length; i++) {
-    var totalPrice = orderDetail[i].totalPrice;
-    var b = (totalPrice / 100).toFixed(2).split('.');
-    orderDetail[i].zs = b[0];
-    orderDetail[i].xs = b[1];
-  }
-
-  that.setData({ obj: res.data.data });
-  console.log(res.data.data);
-}
