0f44c71cdce550ea76d22a4403486d6293cdfc33
Administrator <997797281@qq.com>
2020-01-16 19:02:54 +0800

    123

--- miniprogram/pages/cook/cook.js
+++ miniprogram/pages/cook/cook.js
@@ -1,6 +1,20 @@
+const app = getApp();
 Page({
 
   data: {
-   
+   products:[]
+  },
+  onLoad:function(){
+    var me = this;
+    wx.request({
+      url: app.baseUrl + '/products',
+      method: "GET",
+      success: function (data) {
+        console.log(data.data.data);
+        me.setData({
+          products: data.data.data
+        })
+      }
+    });
   }
 })
